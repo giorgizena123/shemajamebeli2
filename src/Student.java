f7fddb18b480dc2df8559e16e5f90de5a9260646
giorgizena123 <zenaishvili621@gmail.com>
2025-03-14 19:28:40 +0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
+import java.util.Arrays;
+public class Student implements Serializable{
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int year;
+    private double gpa;
+    private String[] subjects;
+    public Student(int id,String firstName,String lastName,int year,double gpa,String subjects){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.year=year;
+        this.gpa=gpa;
+        this.subjects=subjects.split(" ");}
+    @Override
+    public String toString(){
+        return "Student{"+"id="+id+",firstName='"+firstName+'\''+",lastName='"+lastName+'\''+", year="+year+", gpa="+
+                ", subjects"+Arrays.toString(subjects)+'}';
 
-public class Student {
+    }
 }
